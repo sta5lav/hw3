52722c0a2bc49523a6b61053ebcb9ff0f5c34c49
Stanislav <stdu92@mail.ru>
2023-02-15 20:53:32 +0300

    Переменные. Урок 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
+        task5();
     }
 
     public static void task1 () {
@@ -28,5 +31,36 @@ public class Main {
         short five = -159;
         int six = 27897;
         byte seven = 67;
+    }
+    public static void task3 () {
+        int a = 23;
+        int b = 27;
+        int c = 30;
+        int paper = 480;
+        int result = paper / (a+b+c);
+        System.out.println("На каждого ученика рассчитано " + result + " листов бумаги");
+    }
+    public static void task4 () {
+        int botle = 16;
+        int time = 2;
+        int botlePerMinutes = botle / time;
+        int forTwenty = botlePerMinutes * 20;
+        int forDay = botlePerMinutes * 60 * 24;
+        int forThreeDays = forDay * 3;
+        int forMonth = forDay * 30;
+        System.out.println("За " + "20 минут" + " машина произвела " + forTwenty + " бутылок");
+        System.out.println("За " + "сутки" + " машина произвела " + forDay + " бутылок");
+        System.out.println("За " + "3 дня" + " машина произвела " + forThreeDays + " бутылок");
+        System.out.println("За " + "месяц" + " машина произвела " + forMonth + " бутылок");
+    }
+    public static void task5 () {
+        int full = 120;
+        int white = 2;
+        int brown = 4;
+        int perClass = white + brown;
+        int classes = full / perClass;
+        int whiteAll = white * classes;
+        int brownAll = brown * classes;
+        System.out.println("В школе, где " + classes + " классов, нужно " + whiteAll + " банок белой краски и " + brownAll + " банок коричневой краски");
     }
 }
