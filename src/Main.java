68b767c5f5fbd54ad417716ca7f3991db911f1db
Stanislav <stdu92@mail.ru>
2023-02-15 20:36:16 +0300

    Переменные. Урок 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        int oneType = 1;
+        byte twoType = 15;
+        short threeType = 25;
+        long fourtype = 35;
+        float fiveType = 45.5f;
+        double sixType = 55.4;
+        System.out.println("Значение переменной" + " oneType" + " с типом" + " int" + " равно " + oneType);
+        System.out.println("Значение переменной" + " twoType" + " с типом" + " byte" + " равно " + twoType);
+        System.out.println("Значение переменной" + " threeType" + " с типом" + " short" + " равно " + threeType);
+        System.out.println("Значение переменной" + " fourtype" + " с типом" + " long" + " равно " + fourtype);
+        System.out.println("Значение переменной" + " fiveType" + " с типом" + " float" + " равно " + fiveType);
+        System.out.println("Значение переменной" + " sixType" + " с типом" + " double" + " равно " + sixType);
+
+    }
+
+    public static void task2 () {
+        float one = 27.12f;
+        long two = 987678965549l;
+        float three = 2.786f;
+        short four = 569;
+        short five = -159;
+        int six = 27897;
+        byte seven = 67;
     }
 }
